aa82df6bf0ed2167d68a944b7650d39990de09a4
cybermazinho <bryant0rr3sribeiro@gmail.com>
2024-02-07 22:29:50 -0300

    feat: api basica para aula

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
+
+const array = []
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    res.status(200).send('Online!').end()
 })
+
+app.post('/', (req, res) => {
+    array.push(req.body)
+    res.status(200).json({ message: "Usuario Cadastrado!"}).end()
+
+})
+
+app.get('/', (req, res) => {
+    res.status(201).json(array).end()
+
+})
+
+
 app.listen(process.env.PORT || 3000)
